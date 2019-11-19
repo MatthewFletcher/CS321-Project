709c07c15cbd03241745b3bbfb200b6f12dc4db9
Fletcher <bassmattf@gmail.com>
2019-11-19 09:54:53 -0600

    Adding dummy scraper for compiling

---
+++ src/Scraper.java
@@ -0,0 +1,12 @@
+public class Scraper{
+    Scraper()
+    {
+        System.out.println("Scraper called");
+    }
+    public static void main(String args[])
+    {
+        System.out.println("Scraper main.");
+        return;
+    }
+}
+
