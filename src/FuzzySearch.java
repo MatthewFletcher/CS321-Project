b4b7f98420c8087f85aa7d8b9a1c3ebc2cb44761
Fletcher <bassmattf@gmail.com>
2019-11-19 09:59:34 -0600

    Implementing as singleton

--- src/FuzzySearch.java
+++ src/FuzzySearch.java
@@ -1,5 +1,14 @@
 public class FuzzySearch{
     
+    private static FuzzySearch theInstance = new FuzzySearch();
+
+    private FuzzySearch(){};
+
+    public static FuzzySearch getInstance()
+    {
+        return theInstance;
+    }
+
     void Search(String searchterm)
     {
         System.out.printf("Searching with term ->%s<-", searchterm);
