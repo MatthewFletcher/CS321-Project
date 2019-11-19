33135f5e09f263cecd72ff8432cb5d8babc5efd5
Fletcher <bassmattf@gmail.com>
2019-11-19 11:29:06 -0600

    Adding dummy scrape method

--- src/Scraper.java
+++ src/Scraper.java
@@ -1,7 +1,15 @@
+//TODO import Movie
+
 public class Scraper{
     Scraper()
     {
         System.out.println("Scraper called");
+    }
+    public Movie scrape()
+    {
+
+        System.out.println("SCRAPING");
+        return Movie m;
     }
     public static void main(String args[])
     {
