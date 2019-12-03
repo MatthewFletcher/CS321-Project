b5eae2ebf61d1abbed10fe9bf92e1d05368a90c2
Erik Failing <35756586+ewfCode@users.noreply.github.com>
2019-12-02 20:47:37 -0600

    Comments and code cleanup in mainview

--- src/MainView.java
+++ src/MainView.java
@@ -6,10 +6,6 @@ import java.awt.event.WindowEvent;
 public class MainView extends JFrame
 {
     private static MainView instance;
-    private JPanel titleBackground;
-    private JLabel title;
-    private Image titleImage;
-
 
     /**
      * Main function in the application
@@ -20,40 +16,46 @@ public class MainView extends JFrame
     {
         MainView.getInstance();
         System.out.println("BUILD SUCCESS!");
-
     }
 
     /**
      * Default constructor
+     * Builds the main framework for other GUI panels and assets to be placed into
+     * Creates the title panel and label
+     * Initializes SearchView and ResultsView
+     * Uses no layout manager so GUI assets can be placed at exact coordinates
      */
     private MainView()
     {
+        //Setting the JFrame settings
         setTitle("Film Finder");
         setLayout(null);
 
-        //Title Stuff
-        title = new JLabel();
+        //Initializing and placing the label that contains the title image
+        //Also initializing the title image
+        JLabel title = new JLabel();
         String titlePath = new String("images/TitleNoNames.png");
         ImageIcon titleIcon = new ImageIcon(titlePath);
-        titleImage = titleIcon.getImage();
-        titleImage = titleImage.getScaledInstance(267, 90, Image.SCALE_SMOOTH); //create a version of the poster scaled to the size of the JLabel
+        Image titleImage = titleIcon.getImage();
+        titleImage = titleImage.getScaledInstance(267, 90, Image.SCALE_SMOOTH);
         title.setBounds(5,5, 267, 90);
         title.setIcon(new ImageIcon(titleImage)); //set the scaled poster in the JLabel
         add(title);
 
-        //Title background stuff
-        titleBackground = new JPanel();
+        //Initializes an aesthetic background panel for the title label to be placed upon
+        JPanel titleBackground = new JPanel();
         titleBackground.setBackground(Color.LIGHT_GRAY);
         titleBackground.setBounds(5,5, 267, 90);
         titleBackground.setBorder(BorderFactory.createLineBorder(Color.BLACK, 3));
         add(titleBackground);
 
+        //Initializing ResultsView and SearchView panels
         add(SearchView.getInstance());
         add(ResultsView.getInstance());
 
+        //Setting more JFrame settings
         setSize(800,500);//400 width and 500 height
         setVisible(true);//making the frame visible
-
         setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         addWindowListener(new WindowAdapter()
         {
@@ -63,7 +65,6 @@ public class MainView extends JFrame
                 System.exit(0);
             }
         });
-
     }
 
     /**
